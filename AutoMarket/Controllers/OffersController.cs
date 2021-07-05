2ed13e1c38341dfc17a8ed60ce06504e1b744ff7
BlagoKolev <blagovest.kolev@gmail.com>
2021-07-05 06:34:12 +0300

    Create OffersController and OffersView

---
+++ AutoMarket/Controllers/OffersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoMarket.Controllers
+{
+    public class OffersController : Controller
+    {
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+    }
+}
