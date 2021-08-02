61329912a6e84724a81836e1300ae3619f052532
BlagoKolev <blagovest.kolev@gmail.com>
2021-08-02 15:34:15 +0300

    Improve Dealers searching to show all Dealers Offers if no value is selected.

--- AutoMarket/Services/DealersService.cs
+++ AutoMarket/Services/DealersService.cs
@@ -23,34 +23,70 @@ namespace AutoMarket.Services
 
         public ICollection<MyOffersViewModel> GetDealersOffersByName(string dealerName, int id, int itemsPerPage)
         {
-            var dealerId = GetDealerId(dealerName);
+            var vehicleOffers = new List<MyOffersViewModel>();
+            var partOffers = new List<MyOffersViewModel>();
 
-            var vehicleOffers = this.db.VehicleOffers
-                .Where(x => x.ApplicationUser.DealerId == dealerId && x.IsDeleted == false)
-                .OrderByDescending(x => x.CreatedOn)
-                .Select(x => new MyOffersViewModel
-                {
-                    Id = x.Id,
-                    Make = x.Vehicle.Make,
-                    Model = x.Vehicle.Model,
-                    Color = x.Vehicle.Color,
-                    Price = x.Price,
-                    Image = GlobalConstants.VehicleImagePath + x.Pictures.FirstOrDefault().Id + '.' + x.Pictures.FirstOrDefault().Extension
-                })
-                .ToList();
+            if (dealerName == null)
+            {
+                vehicleOffers = this.db.VehicleOffers
+                    .Where(x => x.ApplicationUser.IsDealer && x.IsDeleted == false)
+                    .OrderByDescending(x => x.CreatedOn)
+                    .Select(x => new MyOffersViewModel
+                    {
+                        Id = x.Id,
+                        Make = x.Vehicle.Make,
+                        Model = x.Vehicle.Model,
+                        Color = x.Vehicle.Color,
+                        Price = x.Price,
+                        Image = GlobalConstants.VehicleImagePath + x.Pictures.FirstOrDefault().Id + '.' + x.Pictures.FirstOrDefault().Extension
+                    })
+                    .ToList();
 
-            var partOffers = this.db.PartOffers
-                .Where(x => x.ApplicationUser.DealerId == dealerId && x.IsDeleted == false)
-                .OrderByDescending(x => x.CreatedOn)
-                .Select(x => new MyOffersViewModel
-                {
-                    Id = x.Id,
-                    Name = x.Part.Name,
-                    Status = x.Part.Status,
-                    Price = x.Price,
-                    Image = GlobalConstants.PartImagePath + x.Pictures.FirstOrDefault().Id + '.' + x.Pictures.FirstOrDefault().Extension
-                })
-                .ToList();
+                partOffers = this.db.PartOffers
+                   .Where(x => x.ApplicationUser.IsDealer && x.IsDeleted == false)
+                   .OrderByDescending(x => x.CreatedOn)
+                   .Select(x => new MyOffersViewModel
+                   {
+                       Id = x.Id,
+                       Name = x.Part.Name,
+                       Status = x.Part.Status,
+                       Price = x.Price,
+                       Image = GlobalConstants.PartImagePath + x.Pictures.FirstOrDefault().Id + '.' + x.Pictures.FirstOrDefault().Extension
+                   })
+                   .ToList();
+            }
+            else
+            {
+                var dealerId = GetDealerId(dealerName);
+
+                 vehicleOffers = this.db.VehicleOffers
+                    .Where(x => x.ApplicationUser.DealerId == dealerId && x.IsDeleted == false)
+                    .OrderByDescending(x => x.CreatedOn)
+                    .Select(x => new MyOffersViewModel
+                    {
+                        Id = x.Id,
+                        Make = x.Vehicle.Make,
+                        Model = x.Vehicle.Model,
+                        Color = x.Vehicle.Color,
+                        Price = x.Price,
+                        Image = GlobalConstants.VehicleImagePath + x.Pictures.FirstOrDefault().Id + '.' + x.Pictures.FirstOrDefault().Extension
+                    })
+                    .ToList();
+
+                 partOffers = this.db.PartOffers
+                    .Where(x => x.ApplicationUser.DealerId == dealerId && x.IsDeleted == false)
+                    .OrderByDescending(x => x.CreatedOn)
+                    .Select(x => new MyOffersViewModel
+                    {
+                        Id = x.Id,
+                        Name = x.Part.Name,
+                        Status = x.Part.Status,
+                        Price = x.Price,
+                        Image = GlobalConstants.PartImagePath + x.Pictures.FirstOrDefault().Id + '.' + x.Pictures.FirstOrDefault().Extension
+                    })
+                    .ToList();
+            }
+           
 
             var dealerOffers = new List<MyOffersViewModel>();
             dealerOffers.AddRange(vehicleOffers);
