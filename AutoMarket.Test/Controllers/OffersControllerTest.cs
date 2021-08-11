fb42bdf381326d9d24f230a8fd6f40b96a08cc7f
BlagoKolev <blagovest.kolev@gmail.com>
2021-08-11 12:08:19 +0300

    All Actions in OffersController are tested.

--- AutoMarket.Test/Controllers/OffersControllerTest.cs
+++ AutoMarket.Test/Controllers/OffersControllerTest.cs
@@ -1,10 +1,12 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 using MyTested.AspNetCore.Mvc;
 using AutoMarket.Controllers;
 using AutoMarket.Models.Offers;
-using System;
 using AutoMarket.Data.Models.Enum;
 using AutoMarket.Data.Models;
+using AutoMarket.Models.Parts;
+using System.Linq;
 
 namespace AutoMarket.Test.Controllers
 {
@@ -29,7 +31,7 @@ namespace AutoMarket.Test.Controllers
         public void VehicleDetailsShouldReturnViewWithCorrectModelAndHasAuthorizeAttribute(string offerId)
         {
             MyController<OffersController>
-            .Instance(c => c.WithData(GetFakeOffer()))
+            .Instance(c => c.WithData(GetFakeVehicleOffer()))
             .Calling(c => c.VehicleDetails(offerId))
             .ShouldHave()
             .ActionAttributes(attr => attr.RestrictingForAuthorizedRequests())
@@ -43,7 +45,7 @@ namespace AutoMarket.Test.Controllers
         public void EditVehicleGetShouldReturnViewWithCorrectModelAndShouldHaveAuthorizeAttribute(string offerId)
         {
             MyController<OffersController>
-            .Instance(c => c.WithData(GetFakeOffer()))
+            .Instance(c => c.WithData(GetFakeVehicleOffer()))
             .Calling(c => c.EditVehicle(offerId))
             .ShouldHave()
             .ActionAttributes(attr => attr.RestrictingForAuthorizedRequests())
@@ -70,7 +72,7 @@ namespace AutoMarket.Test.Controllers
         {
 
             MyController<OffersController>
-            .Instance(c => c.WithData(GetFakeOffer()))
+            .Instance(c => c.WithData(GetFakeVehicleOffer()))
             .Calling(c => c.EditVehicle(offerId, With.Default<EditVehicleOfferViewModel>()))
             .ShouldHave()
             .ActionAttributes(attr => attr.RestrictingForAuthorizedRequests().RestrictingForHttpMethod(HttpMethod.Post))
@@ -86,14 +88,81 @@ namespace AutoMarket.Test.Controllers
             MyController<OffersController>
               .Instance()
               .WithSetup(controller => controller.ModelState.AddModelError("TestError", "TestErrorMsg"))
-              .Calling(c => c.EditVehicle(offerId,With.Default<EditVehicleOfferViewModel>()))
+              .Calling(c => c.EditVehicle(offerId, With.Default<EditVehicleOfferViewModel>()))
               .ShouldReturn()
               .View(view => view.WithModelOfType<EditVehicleOfferViewModel>());
-         }
+        }
 
+        [Theory]
+        [InlineData("someFakeId")]
+        public void PartDetailsShouldReturnViewWithCorrectModelAndHaveAuthorizeAttribute(string offerId)
+        {
+            MyController<OffersController>
+                .Instance(c => c.WithData(GetFakePartOffer()))
+                .Calling(c => c.PartDetails(offerId))
+                .ShouldHave()
+                .ActionAttributes(a => a.RestrictingForAuthorizedRequests())
+                .AndAlso()
+                .ShouldReturn()
+                .View(view => view.WithModelOfType<PartDetailsViewModel>());
+        }
 
+        [Theory]
+        [InlineData("someFakeId")]
+        public void EditPartGetShouldReturnViewWithCorrectModelAndShouldHaveAuthorizeAttribute(string offerId)
+        {
+            MyController<OffersController>
+                .Instance(c => c.WithData(GetFakePartOffer()))
+                .Calling(c => c.EditPart(offerId))
+                .ShouldHave()
+                .ActionAttributes(attr => attr.RestrictingForAuthorizedRequests())
+                .AndAlso()
+                .ShouldReturn()
+                .View(view => view.WithModelOfType<EditPartOfferViewModel>());
+        }
 
-        private static VehicleOffer GetFakeOffer()
+        //[Theory]
+        //[InlineData("someFakeId")]
+        //public void EditPartPostShouldHaveAuthorizeAttributeAndShouldReturnRedirectToAction(string offerId)
+        //{
+        //    MyController<OffersController>
+        //        .Instance(c => c.WithData(GetFakePartOffer()))
+        //        .Calling(c => c.EditPart(offerId, With.Default<EditPartOfferViewModel>()))
+        //        .ShouldHave()
+        //        .ActionAttributes(attr => attr.RestrictingForAuthorizedRequests()
+        //        .RestrictingForHttpMethod(HttpMethod.Post))
+        //        .AndAlso()
+        //        .ShouldReturn()
+        //        .RedirectToAction("PartDetails");
+        //}
+
+        [Theory]
+        [InlineData("someFakeId")]
+        public void EditPartPostShouldReturnViewIfModelstateIsNotValid(string offerId)
+        {
+            MyController<OffersController>
+                .Instance()
+                .WithSetup(controller => controller.ModelState.AddModelError("TestError", "TestErrorMsg"))
+                .Calling(c => c.EditPart(offerId, With.Default<EditPartOfferViewModel>()))
+                .ShouldReturn()
+                .View(view => view.WithModelOfType<EditPartOfferViewModel>());
+        }
+
+        [Theory]
+        [InlineData("someFakeId")]
+        public void DeleteShouldHaveAuthorizeAttributeAndShouldRedirectToAction(string offerId)
+        {
+            MyController<OffersController>
+                .Instance(c => c.WithData(GetFakePartOffer()))
+                .Calling(c => c.Delete(offerId))
+                .ShouldHave()
+                .ActionAttributes(attr => attr.RestrictingForAuthorizedRequests())
+                .AndAlso()
+                .ShouldReturn()
+                .RedirectToAction("All");
+        }
+
+        private static VehicleOffer GetFakeVehicleOffer()
         {
             return new VehicleOffer
             {
@@ -114,6 +183,27 @@ namespace AutoMarket.Test.Controllers
                     Transmission = Enum.Parse<TransmissionType>("Manual"),
                     ManufacturingYear = 2005,
                     Мileage = 226000,
+                }
+            };
+        }
+
+        private static PartOffer GetFakePartOffer()
+        {
+            return new PartOffer
+            {
+                Id = "someFakeId",
+                Location = "Sofia",
+                Phone = "0896556633",
+                Price = 10000,
+                Title = "fakeTitle",
+                VehicleType = Enum.Parse<VehicleCategory>("Automobile"),
+                Description = "fakeDescription",
+                Email = "fakeEmail@mail.bg",
+                Part = new Part
+                {
+                    Name = "Clutch",
+                    PartCategory = Enum.Parse<PartCategory>("Engine"),
+                    Status = Enum.Parse<PartStatus>("New")
                 }
             };
         }
