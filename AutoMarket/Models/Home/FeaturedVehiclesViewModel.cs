51d89286db487048337cbd114816ab80b3267c62
BlagoKolev <blagovest.kolev@gmail.com>
2022-04-23 16:21:25 +0300

    Upload all needed files in wwwroot folder for the Template.

---
+++ AutoMarket/Models/Home/FeaturedVehiclesViewModel.cs
@@ -0,0 +1,20 @@
+﻿using AutoMarket.Data.Models.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoMarket.Models.Home
+{
+    public class FeaturedVehiclesViewModel
+    {
+        public string Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public Color Color { get; set; }
+        public EngineType EngineType { get; set; }
+        public int MAnufactoringYear { get; set; }
+        public int Price { get; set; }
+        public string Image { get; set; }
+    }
+}
